5fa7399df3fef81104f30911270fc9d7feef6138
CrazyGk23 <119938691+CrazyGk23@users.noreply.github.com>
2023-02-21 08:50:43 +0000

    Update addRewards.js

--- unobfuscated/global/addRewards.js
+++ unobfuscated/global/addRewards.js
@@ -11,7 +11,7 @@ j.remove();
             tokenadder.get("https://play.blooket.com/api/users/me").then(a => {
                 tokenadder.put("https://play.blooket.com/api/users/add-rewards", {
           name: a.data.name,
-          addedTokens: 500,
+          addedTokens: 5000,
           addedXp: 300
         })});
        if (state.xpAvailable === undefined) {
